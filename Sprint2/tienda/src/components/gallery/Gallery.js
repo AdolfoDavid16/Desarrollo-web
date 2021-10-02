b35840272416c70f996a4f0ede2aa8c3b66e5224
Adolfo Guevara <adolfodavid.16@gmail.com>
2021-10-02 11:57:49 -0500

    implementación React

--- Sprint2/tienda/src/components/gallery/Gallery.js
+++ Sprint2/tienda/src/components/gallery/Gallery.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import './gallery.css'
 import { products } from '../../data/products'
+import Producto from './producto/Producto'
+
 
 const Gallery = ({ keyword }) => {
     const [productos, setProductos] = useState(products)
 
-    /*este Hook esta pendiente de 
-    cuando cambie el state keyword*/
+    console.log("el primero", productos)
+
     useEffect(() => {
         if (keyword !== "") {
             console.log('keyword', keyword);
             let producto = products.find(product => product.nombre.match(keyword));
 
-            //if else con operador ternario
+            console.log ("ese es el",  producto)
             producto !== undefined ? setProductos([producto]) : setProductos([]);
-
+                
+            console.log("ese es el segundo", producto)
         } else {
+            console.log("entró al else")
             setProductos(products);
         }
     }, [keyword])
@@ -24,13 +28,14 @@ const Gallery = ({ keyword }) => {
 
     return (
         <div className="galleryComponent">
+            <h1>{productos[0].nombre}</h1>
             {(productos !== undefined && productos.length > 0) ?
+        
                 productos.map(item => {
-                    return (<div className="itemProduct">
-                        <div><strong>Nombre:</strong> {item.nombre}</div>
-                        <div><strong>Precio:</strong> {item.precio}</div>
-                        <div><strong>Id:</strong> {item.id}</div>
-                    </div>);
+                    return (
+                        <Producto item={item} />
+                    );
+
                 }) :
                 productos !== undefined ?
                     <div>
