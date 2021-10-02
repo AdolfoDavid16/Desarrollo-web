f36b272e28b90a0541a2095ce661c7ddc8d1345c
Adolfo Guevara <adolfodavid.16@gmail.com>
2021-10-02 09:56:53 -0500

    rama de tienda virtual

---
+++ Sprint2/tienda/src/data/Products.js
@@ -0,0 +1,7 @@
+export const products = [{ nombre: "laptop 15.6 pulgadas", precio: 1800000, id: 121 },
+{ nombre: "audifonos gamer", precio: 150000, id: 122 },
+{ nombre: "teclado retroiluminado", precio: 250000, id: 123 },
+{ nombre: "silla gamer", precio: 600000, id: 124 },
+{ nombre: "monitor 21 pulgadas", precio: 550000, id: 125 },
+{ nombre: "mouse", precio: 25000, id: 126 }]
+
